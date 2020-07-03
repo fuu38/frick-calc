63baafd4db340caf00123a82f09aad0f12affe46
Chaha1n <Chaha1n@users.noreply.github.com>
2020-07-03 22:05:34 +0900

    ホームへ戻るボタンの挙動を追加

--- www/js/setting.js
+++ www/js/setting.js
@@ -2,6 +2,31 @@
 「設定を保存」ボタンで設定を反映し、戻る
 //設定が保存されました通知
 */
-document.getElementById('update_Button').onclick = function() {
-    window.location.href = "index.html"
+function doSomething() {
+    return new Promise(function(resolve, reject) {
+        /*Setttingのラジオボタンの値をすべて読み取り、セットする
+        もしエラーを吐いたらreject()を呼ぶ
+        */
+        resolve("設定の保存に成功しました！")
+    })
+
 }
+
+function setSetting() {
+    doSomething()
+        .then(successCallback, failureCallback)
+        .finally(() => {
+            window.location.href = 'index.html'
+        })
+}
+
+function successCallback(arg) {
+    alert(arg);
+}
+
+function failureCallback(err) {
+    console.log(err);
+    alert("保存に失敗しました。\ n何度も繰り返すようであれば、制作者にご連絡ください。 ");
+}
+
+document.getElementById('update_Button').addEventListener('click', setSetting);
