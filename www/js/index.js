ef0fb3b6b27f90805a51a34793b0d098a9a52922
Chaha1n <Chaha1n@users.noreply.github.com>
2020-07-14 15:57:44 +0900

    5_9ボタンの挙動を実装

--- www/js/index.js
+++ www/js/index.js
@@ -21,6 +21,7 @@
 フリックの実装
 その他もろもろ*/
 //設定ボタン
+window.standard = 30; //フリック検知距離　設定で弄れるように
 var ansBox = document.getElementById("ansBox");
 var formula = "";
 
@@ -31,19 +32,61 @@ window.onload = () => {
         window.location.href = "setting.html"
     });
     //5ボタン
-    $("#5_9").on('click', function() {
-
-    });
-    $("#5_9").on('touchstart', function() {
+    $("#5_9").on('touchstart', function(event) {
+        window.touchStartX = event.changedTouches[0].pageX;
+        window.touchStartY = event.changedTouches[0].pageY;
+        console.log(window.touchStartX);
+        console.log(window.touchStartY);
         $("#5_9").css("display", "none");
         $("#5_9_pushed").css("display", "block");
         $("#suggest_5").css("display", "block");
     });
+    $("#5_9").on('touchmove', function(event) {
+        var thisx = event.changedTouches[0].pageX;
+        var thisy = event.changedTouches[0].pageY;
+        const diffX = thisx - window.touchStartX;
+        const diffY = thisy - window.touchStartY;
+        console.log(diffX);
+        console.log(diffY);
+
+        $(".suggest-img").css("display", "none"); //一回サジェストを全部消す
+        if (Math.abs(diffX) < window.standard && Math.abs(diffY) < window.standard) {
+            $("#suggest_5").css("display", "block");
+        } else if (Math.abs(diffX) > Math.abs(diffY) && Math.abs(diffX) !== diffX) { //左フリック
+            $("#suggest_6").css("display", "block");
+        } else if (Math.abs(diffX) > Math.abs(diffY) && Math.abs(diffX) === diffX) { //右フリック
+            $("#suggest_8").css("display", "block");
+        } else if (Math.abs(diffX) <= Math.abs(diffY) && Math.abs(diffY) === diffY) { //上フリック
+            $("#suggest_9").css("display", "block");
+        } else { //上フリック
+            $("#suggest_7").css("display", "block");
+        }
+    });
     $("#5_9").on('touchend', function() {
+        $(".suggest-img").css("display", "none"); //サジェスト削除
+        var thisx = event.changedTouches[0].pageX;
+        var thisy = event.changedTouches[0].pageY;
+        const diffX = thisx - window.touchStartX;
+        const diffY = thisy - window.touchStartY;
+        console.log(diffX);
+        console.log(diffY);
+        if (Math.abs(diffX) < window.standard && Math.abs(diffY) < window.standard) {
+            $("#suggest_5").css("display", "block");
+        } else if (Math.abs(diffX) > Math.abs(diffY) && Math.abs(diffX) !== diffX) { //左フリック
+            addFormula('6');
+        } else if (Math.abs(diffX) > Math.abs(diffY) && Math.abs(diffX) === diffX) { //右フリック
+            addFormula('8');
+        } else if (Math.abs(diffX) <= Math.abs(diffY) && Math.abs(diffY) === diffY) { //上フリック
+            addFormula('9');
+        } else { //下フリック
+            addFormula('7');
+        }
+        $(".suggest-img").css("display", "none");
+        window.touchStartX = 0;
+        window.touxhStartY = 0;
         $("#5_9_pushed").css("display", "none");
         $("#5_9").css("display", "block");
-        $("#suggest_5").css("display", "none");
-        addFormula('5');
+        console.log("func end!");
     });
     //0ボタン
     $("#0_4").on('click', function() {
