d3d5449649d6a1d7303124a607e583b002339569
Chaha1n <chahaaaaan@gmail.com>
2020-10-01 18:27:02 +0900

    fixed some bugs

--- platforms/android/app/src/main/assets/www/js/setting.js
+++ platforms/android/app/src/main/assets/www/js/setting.js
@@ -1,8 +1,12 @@
 /*設定から元の画面へ戻るボタン(設定を何もせず戻る)
 「設定を保存」ボタンで設定を反映し、戻る*/
 //設定が保存されました通知
-window.onload = function() {
-    $("#backToCalcBtn").on("click", function() {
+
+window.onload = function () {
+    $("#backToCalcBtn").on("click", function () {
         window.location.href = "index.html";
+        const settings = document.getElementById("settings-form");
+        alert(settings.handside.value);
+        window.localStorage.setItem("handside", settings.handside.value);
     });
 };
